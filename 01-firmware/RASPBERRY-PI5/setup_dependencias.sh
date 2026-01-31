222533ff593c07be37677f30a0383bf9f076648a
ThePKX <clashgamergames3@gmail.com>
2026-01-31 12:47:20 +0100

    Creado sh de descarga de la libreria necesaria, c++ y python

--- 01-firmware/RASPBERRY-PI5/setup_dependencias.sh
+++ 01-firmware/RASPBERRY-PI5/setup_dependencias.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 sudo apt update
+sudo apt install python3
+sudo apt install build-essential -y
 sudo apt install gpiod libgpiod-dev
 
 echo "Para compilar: g++ main.cpp -o my_gpio_app -lgpiodcxx"
