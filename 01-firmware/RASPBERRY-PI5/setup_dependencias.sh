52f708ec19dc0e5d7fc75ea33c60d2c20964553e
ThePKX <clashgamergames3@gmail.com>
2026-02-02 10:40:31 +0100

    ESP32Servo libreria subida

--- 01-firmware/RASPBERRY-PI5/setup_dependencias.sh
+++ 01-firmware/RASPBERRY-PI5/setup_dependencias.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install gpiod libgpiod-dev
+sudo apt install python3
+sudo apt install build-essential -y
 
 echo "Para compilar: g++ main.cpp -o my_gpio_app -lgpiodcxx"
